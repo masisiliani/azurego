191584c8f5c198446cc1f5fbf203aca81d482ecc
msisiliani <marcela.silva@resultadosdigitais.com.br>
2019-09-20 22:19:52 -0300

    Add comments on main.go

--- main.go
+++ main.go
@@ -4,16 +4,23 @@ import (
 	"net/http"
 )
 
+//main: função principal do programa
 func main() {
+	//Criamos um server em Go
 	mux := http.NewServeMux()
-	mux.HandleFunc("/devas", Desenvolvedores)
+	//Criamos uma rota nova, que vai ser chamada em /devas utilizando como resposta o método Desenvolvedoras
+	mux.HandleFunc("/devas", Desenvolvedoras)
 
+	//Configuramos para nosso server responder na porta 5000, ou seja, http://localhost:5000/[rota]
 	http.ListenAndServe(":5000", mux)
 }
 
 func Desenvolvedoras(w http.ResponseWriter, r *http.Request) {
+	// Adicionamos um header ao nosso response, informando que o content type retornado vai ser em json
 	w.Header().Set("Content-Type", "application/json")
 
+	//criamos uma listagem, estática, de objetos
+	//Aqui é construida a listagem que vamos retornar na nossa API
 	jsonStr := `[{"name":"Silvana","age":29,"favlang":"Vue", "twitter":"@silcrana"}, 
 				{"name":"Marcela","age":31,"favlang":"Golang","twitter":"@masisiliani"},
 				{"name":"Jessica Temporal","age":25,"favlang":"Python","twitter":"@jessTemporal"},
@@ -22,5 +29,6 @@ func Desenvolvedoras(w http.ResponseWriter, r *http.Request) {
 				{"name":"Erika","age":23,"favlang":"Go","twitter":"@erikones_"},
 				{"name":"Jessica Nathany","age":27,"favlang":".NET","twitter":"@JessicaNathanyF"}]`
 
+	//escrevemos a listagem do nosso retorno no nosso response
 	w.Write([]byte(jsonStr))
 }
